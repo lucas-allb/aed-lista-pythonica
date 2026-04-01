52171ebb56f23e968188e6dae5b795d5495a1fd2
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-04-01 17:10:42 -0300

    complete-exercise-06

--- src/exercicio_06.py
+++ src/exercicio_06.py
@@ -1,11 +1,3 @@
 def clean_name(name: str) -> str:
-    """
-    Remove espaços em branco no início e no fim da string.
-
-    Args:
-        name (str): nome com possíveis espaços extras
-
-    Returns:
-        str: nome sem espaços nas extremidades
-    """
-    pass
+    nome = name
+    return f"{nome.strip()}"
