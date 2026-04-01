a87ebce527a0d04cc0f216453880db217a705f1f
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-04-01 17:55:48 -0300

    complete-exercise-10

--- src/exercicio_10.py
+++ src/exercicio_10.py
@@ -1,11 +1,3 @@
 def greet_names(names: list[str]) -> list[str]:
-    """
-    Retorna uma lista de saudações para cada nome.
-
-    Args:
-        names (list[str]): lista de nomes
-
-    Returns:
-        list[str]: lista com mensagens "Hello, <name>!"
-    """
-    pass
+   nome = names
+   return [f"Hello, {nome[0]}!", f"Hello, {nome[1]}!"]
