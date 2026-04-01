1e66496b6e6a572e21e3853eeb92904d52bad1bb
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-04-01 17:42:41 -0300

    complete-exercise-08

--- src/exercicio_08.py
+++ src/exercicio_08.py
@@ -1,11 +1,3 @@
 def favorite_number(n: int) -> str:
-    """
-    Retorna uma frase contendo o número favorito.
-
-    Args:
-        n (int): número favorito
-
-    Returns:
-        str: "Your favorite number is <n>"
-    """
-    pass
+    numero = n
+    return f"Your favorite number is {numero}"
