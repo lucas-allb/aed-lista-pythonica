9b78c7f87c3e32cce58aa7c9669d9c5723ce1817
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-04-01 18:53:33 -0300

    complete-exercise-12

--- src/exercicio_12.py
+++ src/exercicio_12.py
@@ -2,14 +2,6 @@ def add_guests(
     guests: list[str],
     new_guests: list[str]
 ) -> list[str]:
-    """
-    Adiciona múltiplos convidados à lista.
-
-    Args:
-        guests (list[str]): lista original
-        new_guests (list[str]): novos convidados
-
-    Returns:
-        list[str]: lista atualizada
-    """
-    pass
+    guests.extend(new_guests)
+    return guests
+   
