94798906941f6e8be95cc27a8539c31614bc08cc
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-04-01 16:45:42 -0300

    complete-exercise-03

--- src/exercicio_03.py
+++ src/exercicio_03.py
@@ -1,11 +1,3 @@
 def personal_message(name: str) -> str:
-    """
-    Retorna uma saudação personalizada.
-
-    Args:
-        name (str): nome da pessoa
-
-    Returns:
-        str: saudação no formato "Hello, <name>!"
-    """
-    pass
+   nome = name
+   return  f'Hello, {nome}!'
