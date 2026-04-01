4b5b4edeb38b7812a588aa52d6be3730e2d3769d
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-04-01 17:45:26 -0300

    complete-exercise-09

--- src/exercicio_09.py
+++ src/exercicio_09.py
@@ -1,11 +1,6 @@
 def is_even(n: int) -> bool:
-    """
-    Verifica se um número é par.
-
-    Args:
-        n (int): número inteiro
-
-    Returns:
-        bool: True se for par, False caso contrário
-    """
-    pass
+    numero = n
+    if numero %2 == 0:
+        return True
+    else:
+        return False
